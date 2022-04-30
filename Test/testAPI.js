a42b466c7dba0fc563cd0ebe44dfe4921bad908e
goncast <goncast@usi.ch>
2022-04-30 15:12:57 +0200

    Added checks to Endpoint A

--- Test/testAPI.js
+++ Test/testAPI.js
@@ -1,8 +1,19 @@
 // Testing various implementations of the anagram API
 
-// Endpoint A function
+
+
+/**
+ * @param {string} word1 - A single word
+ * @param {string} word2 - A single word
+ * @return {Boolean} A boolean that tells us whether or not the two words sent in the body are anagrams
+ */
 
 function CheckIfAnagram(word1, word2){
+
+    // check if the input is valid, not null, and not empty
+    if(word1 === undefined || word2 === undefined || word1 === null || word2 === null || word1 === '' || word2 === ''){
+        return undefined;
+    }
 
     // check if the words are the same length
     if (word1.length != word2.length) {
@@ -13,7 +24,11 @@ function CheckIfAnagram(word1, word2){
     var firstWord = word1.toLowerCase().split('').sort().join('');
     var secondWord = word2.toLowerCase().split('').sort().join('');
 
+    console.log(firstWord);
+    console.log(secondWord);
+
     // compare the two words and return the result
     return firstWord === secondWord;
 }
 
+console.log(CheckIfAnagram('hel,lo*ç%&.', 'olelh.,*ç%&'));
