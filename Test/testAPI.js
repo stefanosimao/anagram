7aedd2ff7166237b27afe8877bbe1575220ae385
goncast <goncast@usi.ch>
2022-05-04 00:27:44 +0200

    Improved B

--- Test/testAPI.js
+++ Test/testAPI.js
@@ -29,11 +29,6 @@ function CheckIfAnagram(word1, word2){
         return false;
     }
 
-    // check if the words are the same, if so, they are not anagrams
-    if (word1 === word2) {
-        return false;
-    }
-
     // sort the characters in each word
     var firstWord = word1.toLowerCase().split('').sort().join('');
     var secondWord = word2.toLowerCase().split('').sort().join('');
@@ -41,6 +36,7 @@ function CheckIfAnagram(word1, word2){
     // compare the two words and return the result
     return firstWord === secondWord;
 }
+//console.log(CheckIfAnagram('Elvis', 'Lives'));
 
 /**
  * Endpoint B
@@ -66,14 +62,14 @@ function FindAnagrams(word, sentence){
     // loop through the array of words
     for(var i = 0; i < sentenceArray.length; i++){
 
-        // check if the word is an anagram of the word in the body
-        if(CheckIfAnagram(word, sentenceArray[i])){
+        // check if the anagram is already in the array
+        if (anagrams.indexOf(sentenceArray[i]) === -1) {
 
-            // check if the anagram is already in the array
-            if(anagrams.indexOf(sentenceArray[i]) === -1){
+            /// check if the word is an anagram of the word in the body
+            if (CheckIfAnagram(word, sentenceArray[i])) {
 
-                // add the anagram to the array
-                anagrams.push(sentenceArray[i]);
+                // add the anagram to the array and remove all punctuation from the word
+                anagrams.push(sentenceArray[i].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, ""));
             }
         }
     }
@@ -81,6 +77,11 @@ function FindAnagrams(word, sentence){
     // return the array of anagrams if there are any
     return (anagrams.length > 0) ? anagrams : "No anagrams found!";
 }
+// console.log(FindAnagrams("elvis", "elvis lives"));
+
+
+
+
 
 
 /**
@@ -109,18 +110,18 @@ function FindAnagramGroups(sentence){
             // create an array to store the anagrams by calling the FindAnagrams function
             var anagrams = FindAnagrams(sentenceArray[i], sentence);
 
-            // check if the anagram group is already in the array
-            if (anagrams instanceof Array && anagramGroups.indexOf(anagrams) === -1){
-    
+            // check if the anagram array is an array and if the length is greater than 1
+            if (anagrams instanceof Array && anagrams.length > 1) {
+                
                 // add the anagram group to the array
                 anagramGroups.push(anagrams);
+                sentenceArray.splice(sentenceArray.indexOf(sentenceArray[i]),1);
+                console.log(sentenceArray);
             }
         }
     
         // return the array of anagram groups if there are any
         return (anagramGroups.length > 0) ? anagramGroups : "No anagram groups found!";
 }
-
-
-console.log(FindAnagramGroups('Elvis lives in a house. His cat can act. He has a study that’s dusty'));
+//console.log(FindAnagramGroups('Elvis lives in a house. His cat can act. He has a study that’s dusty'));
 
